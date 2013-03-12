f56ed7b4b603606acd8c8fa21dbc9bda54870e47
Katharina Best <katharinalbest@gmail.com>
2013-03-12 15:54:59 -0400

    made changes to work with new R file, but have bug related to unnecessary quotes in some college IDs

---
+++ datamanip_main_underscore.py
@@ -0,0 +1,223 @@
+#import modules========================================================================================
+import sys
+
+#set up global variables===============================================================================
+collegeList = [] #list of colleges
+missedCollegeList = [] #list of colleges without info
+missedSelectivityList = [] #list of schools among 4-year schools for which selectivity is missing
+crosswalkLookup = {} #returns an IPEDS ID for a FICE number
+collegeDataLookup = {} #returns collegeData object for an IPEDS number
+OPEIDcrosswalkLookup = {} #returns an IPEDS ID for an OPEID number
+collegeDataLookup = {} #lookup table storing college info based on IPEDS number
+
+#define classes========================================================================================
+class CollegeData: #class storing college data
+	def __init__(self, colName, bachFlag, control, selectivity):
+		self.colName, self.bachFlag, self.control, self.selectivity = colName, bachFlag, control, selectivity
+
+	def __str__(self):
+		return str(self.colName) + "\t" + str(self.bachFlag) + "\t" + str(self.control) +  "\t" + str(self.selectivity)
+
+#main===================================================================================================
+def main():
+	#get list of colleges for which to extract information
+	collegeListSetup()
+
+	#try to fill in missing colleges using multiple years of IPEDS files and the OPEIDS crosswalk; populatecolelgedatalookup with data that comes from this file
+	IPEDScheck(2004)
+	IPEDScheck(2006)
+	IPEDScheck(2005)
+	IPEDScheck(2003)
+	IPEDScheck(2002)
+	IPEDScheck(2001)
+	IPEDScheck(2011)
+	BarronsSetup() #add barron's selectivity to infor we have about schools
+	
+	#try to replace schools using FICE codes where possible
+	FICEcrosswalkSetup() #set up lookup that returns an IPEDS ID for a FICE number
+	FICEcheck() #do replacement where applicable
+
+	#print college lists for future use
+	printCollegeList()
+
+	#delete colleges from found list that do not indicate having 4-year data
+	deleteNonCollege()
+
+	#merge with matched information--remove non-4-year schools and investigate public/private distinction
+	#get info at http://nces.ed.gov/ipeds/datacenter/InstitutionByName.aspx?stepId=1
+
+	#get Barron's numbers
+
+	#populate accepted set for each individual (must be correct school type)
+
+	#check that attended school was accepted
+
+	#set highest accepted flag
+
+	#set level of attended flag
+
+#function definitions==============================================================================================
+def collegeListSetup(): #extract list of colleges people have attended
+	global missedCollegeList
+	vectorList = open('D:/compiledcollegelist.txt', 'r')
+	for line in vectorList.readlines():
+		str.replace(line, '"', '')
+		#read in variables
+		varList = line.split('\t')
+		PUBID_1997 = varList[1]
+		COLLEGEGOER_FLAG = varList[2]
+		COLLEGE_SCHOOLID = varList[3]
+		COMPILED_APPLY = varList[4]
+		COMPILED_ADMIT = varList[5]
+		applyVector = COMPILED_APPLY.split(',')
+		admitVector = COMPILED_ADMIT.split(',')
+		for i in range(len(applyVector)):
+			missedCollegeList.append(applyVector[i])
+		for i in range(len(admitVector)):
+			missedCollegeList.append(admitVector[i])
+	vectorList.close()
+	#clean up college list
+	#print "Total number of unique IDs: " + str(len(missedCollegeList))
+	#outFiletest = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc/beforededupe.txt', 'w')
+	#outFiletest.write(str(missedCollegeList))
+	#outFiletest.close()
+	missedCollegeList = list(set(missedCollegeList)) #de-dupe
+	print "Total number of unique IDs: " + str(len(missedCollegeList))
+
+def IPEDScheck(myYear): #look up colleges in the list in myYear's ipeds list and this year's OPEID list; create OPEID lookup table based on this year's data
+	global collegeList
+	global missedCollegeList
+	global OPEIDcrosswalkLookup
+	global collegeDataLookup
+	curIPEDS = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc/hd' + str(myYear) + '.txt', 'rb')
+	#update set of "known" IDs and OPEID lookup table
+	for line in curIPEDS.readlines():
+		varList = line.split('\t')
+		unitID = varList[0]
+		colName = varList[1]
+		opeid = varList[15]
+		curBachFlag = varList[19]
+		curControl = varList[20]
+		if unitID not in collegeDataLookup: #update current known IPED IDs#
+			curSelectivity = -3  #we haven't done this yet
+			curColData = CollegeData(colName, curBachFlag, curControl, curSelectivity)
+			collegeDataLookup[unitID] = curColData
+			#print collegeDataLookup[unitID]
+		if (opeid not in OPEIDcrosswalkLookup): #update OPEID crosswalk
+			OPEIDcrosswalkLookup[opeid]= unitID
+	curIPEDS.close()
+	missedCollegeListCopy = list(missedCollegeList)
+	collegeListCopy = list(collegeList)
+	for i in range(len(missedCollegeList)): #try to ID missed colleges by IPEDS
+		if (missedCollegeList[i]) in collegeDataLookup:
+			collegeListCopy.append(missedCollegeList[i])
+			missedCollegeListCopy.remove(missedCollegeList[i])
+		elif (missedCollegeList[i]) in OPEIDcrosswalkLookup: #try to ID missed colleges by OPEID
+			print "code 1: found college by OPEID lookup in year " + str(myYear) #this happens once
+			collegeListCopy.append(OPEIDcrosswalkLookup[missedCollegeList[i]])
+			missedCollegeListCopy.remove(missedCollegeList[i])
+	missedCollegeList = missedCollegeListCopy
+	collegeList = collegeListCopy
+	collegeList = list(set(collegeList)) #de-dupe
+	missedCollegeList = list(set(missedCollegeList)) #de-dupe
+	#print "Total number of found colleges after searching " + str(myYear) + ": " + str((len(collegeList)))
+
+def FICEcrosswalkSetup(): #set up crosswalk for FICE number
+	global crosswalkLookup
+	crosswalkFile = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc//crosswalkfile.txt', 'r')
+	for line in crosswalkFile.readlines():
+		varList = line.split('\t')
+		unitID = varList[0]
+		name = varList[1]
+		ficeID = varList[2]
+		ficeID = str.replace(ficeID, '\n', '')
+		if (len(ficeID) > 0 and ficeID not in crosswalkLookup):
+			crosswalkLookup[ficeID]= unitID
+	crosswalkFile.close()
+
+def FICEcheck(): #use FICE crosswalk to try to fill in missing information
+	global missedCollegeList
+	global collegeList
+	global crosswalkLookup
+	missedCollegeListCopy = list(missedCollegeList)
+	collegeListCopy = list(collegeList)
+	for i in range(len(missedCollegeList)):
+		if (missedCollegeList[i]) in crosswalkLookup:
+				print "code 2: found college by FICE check" #this doesn't happen
+				collegeListCopy.append(crosswalkLookup[missedCollegeList[i]])
+				missedCollegeListCopy.remove(missedCollegeList[i])
+	missedCollegeList = missedCollegeListCopy
+	collegeList = collegeListCopy
+	collegeList = list(set(collegeList)) #de-dupe
+
+def BarronsSetup(): #lookup for Barron's selectivity
+	barronsFile = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc//barrons04.txt', 'r')
+	for line in barronsFile.readlines():
+		varList = line.split('\t')
+		unitID = varList[1]
+		curSelectivity = varList[8]
+		if unitID in collegeDataLookup:
+			collegeDataLookup[unitID].selectivity = curSelectivity
+	barronsFile.close()
+	barronsFile2 = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc//barrons08.txt', 'r')
+	for line in barronsFile2.readlines(): #there are no cases where 04 has no data and 08 does, but just checking
+		varList = line.split('\t')
+		unitID = varList[1]
+		curSelectivity = varList[8]
+		if unitID in collegeDataLookup and collegeDataLookup[unitID].selectivity == -3:
+			collegeDataLookup[unitID].selectivity = curSelectivity
+	barronsFile2.close()
+
+def printCollegeList():	
+	global collegeList
+	global missedCollegeList
+	global missedSelectivityList
+	for i in collegeList:
+		if collegeDataLookup[i].selectivity == -3:
+			missedSelectivityList.append(i)
+	print "Total number of unique IDs that are found after using all means: " + str((len(collegeList)))
+	print "Total number of entries in missed colleges list: " + str((len(missedCollegeList)))
+	print "Total number of found schools with no selectivity data: " + str((len(missedSelectivityList)))
+	collegeListStr = str(collegeList)
+	collegeListStr= str.replace(collegeListStr, '[', '')
+	collegeListStr= str.replace(collegeListStr, ']', '')
+	collegeListStr= str.replace(collegeListStr, '\'', '')
+	missedCollegeListStr = str(missedCollegeList)
+	missedCollegeListStr= str.replace(missedCollegeListStr, '[', '')
+	missedCollegeListStr= str.replace(missedCollegeListStr, ']', '')
+	missedCollegeListStr= str.replace(missedCollegeListStr, '\'', '')
+	missedSelectivityListStr = str(missedSelectivityList)
+	missedSelectivityListStr= str.replace(missedSelectivityListStr, '[', '')
+	missedSelectivityListStr= str.replace(missedSelectivityListStr, ']', '')
+	missedSelectivityListStr= str.replace(missedSelectivityListStr, '\'', '')
+	outFile = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc/uniqueColleges.txt', 'w')
+	outFile.write(collegeListStr)
+	outFile.close()
+	outFile2 = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc/missedColleges.txt', 'w')
+	outFile2.write(missedCollegeListStr)
+	outFile2.close()
+	outFile3 = open('C:/Users/Katharina/Documents/UMICH/Lifecycle choice/Data/ycoc/missingSelectivity.txt', 'w')
+	outFile3.write(missedSelectivityListStr)
+	outFile3.close()
+
+def deleteNonCollege(): #note that colleges are still in the lookup table, just not in the college lists
+	global collegeList
+	global collegeDataLookup
+	collegeListCopy = list(collegeList)
+	for i in range(len(collegeList)):
+		if collegeDataLookup[collegeList[i]].bachFlag != "1":
+			collegeListCopy.remove(collegeList[i])
+	collegeList = collegeListCopy
+	print "Total number of unique 4-year IDs that are found after using all means: " + str((len(collegeList)))
+	collegeListCopy = list(collegeList)
+	for i in range(len(collegeList)):
+		if collegeDataLookup[collegeList[i]].bachFlag == -3:
+			collegeListCopy.remove(collegeList[i])
+	collegeList = collegeListCopy
+	print "Total number of unique 4-year IDs with selctivity that are found after using all means: " + str((len(collegeList)))
+
+if __name__ == '__main__':
+	main()
+
+
+
