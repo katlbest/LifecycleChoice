10168883379e8e8a9274d9b5d6d7678461860be2
Katharina Best <katharinalbest@gmail.com>
2013-06-13 15:03:02 -0400

    added data simplification, output simple data, and began reading and organizing anonimized data

--- choicemodel.R
+++ choicemodel.R
@@ -56,6 +56,37 @@
         na.dat$distance[i] = sqrt((na.dat$latitude[i] -na.dat$latstudent[i])^2+ (na.dat$longitude[i] - na.dat$longstudent[i])^2)
     }
 
+  #simplified sports variable
+    na.dat$division2 = na.dat$division
+    na.dat[is.na(na.dat$division2),]$division2 = 0
+    na.dat[na.dat$division2>1,]$division2 = 0
+  
+  #missing enrollments and related numbers
+    na.dat[is.na(na.dat$expperstudent),]$instperstudent =c(4520.61506,1150.341232,1504.649105,NA,31187.00164,3394.802632,NA,NA,3891.508412,NA,4945.538738,5024.843797,5871.471399,5871.471399,5871.471399,5871.471399,5871.471399,5867.027995,NA,NA,NA,NA,1367.668782,1367.668782,1367.668782,2250.383634,2250.383634,2275.225379,NA,NA) 
+    na.dat[is.na(na.dat$expperstudent),]$facperstudent = c(0.214457831,0.003041054,0.06361829,NA,0.36078145,0.157894737,NA,NA,0.133243607,NA,0.124743614,0.122571001,0.134990774,0.134990774,0.134990774,0.134990774,0.134990774,0.2421875,NA,NA,NA,NA,0.050761421,0.050761421,0.050761421,0.094404957,0.094404957,0.060606061,NA,NA)
+    na.dat[is.na(na.dat$expperstudent),]$totstudents = c(415,1973,251.5,NA,1369.25,209,2616.25,2616.25,743,NA,5363,334.5,5148.5,5148.5,5148.5,5148.5,5148.5,384,607,306.25,800.5,499,394,394,394,2743.5,2743.5,132,NA,2392)
+    na.dat[is.na(na.dat$expperstudent),]$expperstudent = c(10701.30783,4957.191586,7838.678926,NA,103966.2806,10630.63278,NA,NA,12222.84791,NA,15083.20231,9542.077728,15215.34976,15215.34976,15215.34976,15215.34976,15215.34976,22338.86719,NA,NA,NA,NA,7082.442893,7082.442893,7082.442893,6845.025241,6845.025241,7493.844697,NA,NA)
+    na.dat[na.dat$AdmittedSchool == 186399,]$gradrate = .51
+    na.dat[na.dat$AdmittedSchool == 228644,]$gradrate = 1
+  
+  #simplified carnegie
+    na.dat[is.na(na.dat$carnegie),]$carnegie = c(32, 32, 32, 32, 54, 15)
+    na.dat$carnegie2 = NA
+    for (i in 1:nrow(na.dat)){
+      if (na.dat$carnegie[i] %in% c(15, 16)){
+        na.dat$carnegie2[i] =1
+      } else if(na.dat$carnegie[i] %in% c(22, 21)){
+        na.dat$carnegie2[i] =2
+      } else if(na.dat$carnegie[i] %in% c(31, 32, 33)){
+        na.dat$carnegie2[i] =3
+      } else if(na.dat$carnegie[i] %in% c(40, 51, 52:59)){ #associates schools with some bach, specialty, seminary
+        na.dat$carnegie2[i] =4
+      }
+    }
+
+keyVars = c("PUBID_1997", "AdmittedSchool", "tuioutlist", "realtui", "FINAIDEST", "distance", "instate", "urbanruralmatch", "selectdiff", "loanp", "fedgrantp", "genderratio", "totstudents", "control", "carnegie2", "selectivity2", "expperstudent", "instperstudent", "facperstudent", "avgsal", "division2", "gradrate", "AttendedIndicator")
+out.df = na.dat[,keyVars]
+write.csv(out.df, "D:mainvars.csv")
 
 #preliminary visualization===========================================================
   #correlation and plot
@@ -66,7 +97,7 @@
     attend.dat= attend.dat[,!(colnames(attend.dat) %in% drop)]
     write.csv(cor(na.exclude(attend.dat)), "C:/Users/Katharina/Documents/Umich/Lifecycle Choice/Data/choicemodel/inputcorrincome.csv")
 
-#setup data==========================================================================
+#handle interaction variables==========================================================================
   #create lifetime income estimate
     tau =27.8818
     m = -3.8149
@@ -83,17 +114,13 @@
       }
     }
 
+#ANONYMIZED MODEL SETUP===============================================================
+  #read anon data
+  anon.dat = read.csv("C:/Users/Katharina/Documents/Umich/Lifecycle Choice/Data/Choice Model Inputs/mainvars-anon.csv")
+  #setup model inputs
+    #create choice set information
+      #pubid_anon identifies decision-maker ID
+      #school_anon identifies alternative ID
+      #AttendedIndicator identifies choice made
+      mlogit.anon.dat = mlogit.data(anon.dat, shape = "long", choice = "attendedIndicator", alt.var= "school_anon", chid.var = "pubid_anon")
 
-    
-
-  #select/create relevant variables
-
-  #create choice set information
-    #PUBID_1997 identifies decision-maker ID
-    #AdmittedSchool identifies alternative ID
-    #AttendedIndicator identifies choice made
-    test = na.dat[,c("PUBID_1997","AdmittedSchool","b0","AttendedIndicator")]
-    mlogit.na.dat = mlogit.data(test, shape = "long", choice = "AttendedIndicator", alt.var= "AdmittedSchool", chid.var = "PUBID_1997")
-
-#model of attenders only===============================================================
-  
